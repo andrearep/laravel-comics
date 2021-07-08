7fe9f29970f23abc606906626d75027d8fd7497f
andrearep <andrea.repe@gmail.com>
2021-07-08 18:21:44 +0200

    footer main completato

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -50,9 +50,65 @@
     </header>
 
     <main>
+        @yield('content')
     </main>
 
     <footer>
+        <div class="container">
+            <div class="footer_main">
+                <div class="left_side">
+                    <div class="columns">
+                        <div class="column">
+                            <span class="title">DC comics</span>
+                            <ul>
+                                <li><a href="">characters</a></li>
+                                <li> <a href="">comics</a></li>
+                                <li><a href="">movies</a></li>
+                                <li><a href="">tv</a></li>
+                                <li><a href="">games</a></li>
+                                <li><a href="">videos</a></li>
+                                <li> <a href="">news</a></li>
+                                </li>
+                            </ul>
+                            <span class="title">shop</span>
+                            <ul>
+                                <li><a href="">shop DC</a></li>
+                                <li> <a href="">shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div class="column">
+                            <span class="title">DC</span>
+                            <ul>
+                                <li><a href="">term of use</a></li>
+                                <li> <a href="">privacy policy (new)</a></li>
+                                <li><a href="">ad choices</a></li>
+                                <li><a href="">advertising</a></li>
+                                <li><a href="">jobs</a></li>
+                                <li><a href="">subscriptions</a></li>
+                                <li> <a href="">talent workshop</a></li>
+                                <li> <a href="">CPSC certificates</a></li>
+                                <li> <a href="">ratings</a></li>
+                                <li> <a href="">shop help</a></li>
+                                <li> <a href="">contact us</a></li>
+                            </ul>
+                        </div>
+                        <div class="column">
+                            <span class="title">sites</span>
+                            <ul>
+                                <li><a href="">DC</a></li>
+                                <li> <a href="">MAD Magazine</a></li>
+                                <li><a href="">DC Kids</a></li>
+                                <li><a href="">DC Universe</a></li>
+                                <li><a href="">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="right_side">
+                    
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 
