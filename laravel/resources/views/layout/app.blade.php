b661ef3cab50700a3ca8473e22474093bbc2507c
andrearep <andrea.repe@gmail.com>
2021-07-08 17:20:00 +0200

    inserita cartella img completato header

--- laravel/resources/views/layout/app.blade.php
+++ laravel/resources/views/layout/app.blade.php
@@ -9,6 +9,9 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
@@ -17,7 +20,33 @@
 <body>
 
     <header>
-        <img src="{{ ('img/dc-logo.png') }}" alt="">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </div>
+
+            <nav>
+                <a href="">characers</a>
+                <a href="">comics</a>
+                <a href="">movies</a>
+                <a href="">tv</a>
+                <a href="">games</a>
+                <a href="">collection</a>
+                <a href="">videos</a>
+                <a href="">fans</a>
+                <a href="">news</a>
+                <a href="">shop <i class="fas fa-sort-down"></i></a>
+            </nav>
+
+            <div class="search">
+
+                <input type="text" placeholder="SEARCH">
+                <span><i class="fas fa-search"></i></span>
+
+
+            </div>
+        </div>
+
     </header>
 
     <main>
